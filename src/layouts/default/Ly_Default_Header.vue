c56f3b812e03715e83b4af6924ae1d852800bda0
김영우 <kyw05171@gmail.com>
2021-06-13 15:40:57 +0900

    <김영우>: Ly_Default_Header 의 시멘틱 태그를 수정 하였습니다.
    각 <img>태그를 감싸는 <figure> 태그를 추가 하였습니다.

--- src/layouts/default/Ly_Default_Header.vue
+++ src/layouts/default/Ly_Default_Header.vue
@@ -1,30 +1,42 @@
 <template>
 	<header id="Ly_Default_Header" class="header">
-		<figure class="header__imgWrapper">
+		<div class="header__wrapper">
 			<div class="header__decoVer"></div>
 			<div class="header__decoHor"></div>
 
-			<img
-				src="../../assets/image/header/예방접종-병원.png"
-				alt="이미지: 병원"
-				class="header__hospital"
-			/>
+			<!-- 병원 이미지 -->
+			<figure class="header__hospitalWrapper">
+				<div class="header__hospitalShadow"></div>
 
-			<img
-				src="../../assets/image/header/예방접종-사람.png"
-				alt="이미지: 사람"
-				class="header__person"
-			/>
+				<img
+					src="../../assets/image/header/예방접종-병원.png"
+					alt="이미지: 병원"
+					class="header__hospital"
+				/>
+			</figure>
 
-			<img
-				src="../../assets/image/header/예방접종-바이러스.png"
-				alt="이미지: 바이러스"
-				class="header__virus"
-			/>
+			<!-- 사람 이미지 -->
+			<figure class="header__personWrapper">
+				<div class="header__personShadow"></div>
+
+				<img
+					src="../../assets/image/header/예방접종-사람.png"
+					alt="이미지: 사람"
+					class="header__person"
+				/>
+			</figure>
+
+			<figure class="header__virusWrapper">
+				<img
+					src="../../assets/image/header/예방접종-바이러스.png"
+					alt="이미지: 바이러스"
+					class="header__virus"
+				/>
+			</figure>
 
 			<h1 class="header__title">코로나19 예방접종 센터</h1>
 			<h1 class="header__title">조회 서비스</h1>
-		</figure>
+		</div>
 	</header>
 </template>
 
@@ -50,7 +62,7 @@ export default Vue.extend({
 
 		$hospital-left: 10%;
 
-		&__imgWrapper {
+		&__wrapper {
 			@include width-height(100%, 0);
 
 			padding-top: 300px;
@@ -85,31 +97,57 @@ export default Vue.extend({
 		}
 
 		// 병원 이미지
-		&__hospital {
+		&__hospitalWrapper {
 			@include width-height($hospital-width);
 
 			position: absolute;
 			top: $hospital-left;
 			left: 10%;
 		}
+		&__hospital {
+			//
+		}
 
 		// 사람 이미지
-		&__person {
-			@include width-height($person-width);
+		&__personWrapper {
+			@include width-height($person-width, 130px);
 
 			position: absolute;
 			top: 30%;
 			left: 20%;
 		}
+		&__personShadow {
+			@include width-height(calc(100% + 20px), 10%);
+
+			position: absolute;
+			bottom: 0;
+			left: -10px;
+			transform: translateY(50%);
+
+			background: radial-gradient(
+				circle,
+				rgba($colors__black_02, 1) 0%,
+				rgba($colors__black_02, 0) 100%
+			);
+			border-radius: 50%;
+		}
+		&__person {
+			position: absolute;
+			top: 0;
+			left: 0;
+		}
 
 		// 바이러스 이미지
-		&__virus {
+		&__virusWrapper {
 			@include width-height($virus-width);
 
 			position: absolute;
 			top: 50%;
 			left: calc(20% + #{$person-width});
 			transform: translateY(-50%);
+		}
+		&__virus {
+			//
 		}
 
 		// Header 제목
