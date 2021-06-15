b14e7dabbd0c2c69f3b1e01116e3b73f5f5bbd73
김영우 <kyw05171@gmail.com>
2021-06-15 14:43:29 +0900

    <김영우>: 브라우저 Cookie 의 "SameSite" 설정을 "None" 으로 변경 하였습니다.

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,23 @@ import { initFontAwesome } from "@/plugins/fontawesome/pluginFontawesome";
 Vue.config.productionTip = false;
 initFontAwesome();
 
+// SameSite 설정
+function initSameSiteRule(): void {
+	const sameSiteRule = "SameSite=None; Secure";
+	const cookies = document.cookie;
+
+	if (cookies.indexOf("SameSite") > -1 && cookies.indexOf("Secure") > -1) {
+		return;
+	}
+
+	if (cookies.indexOf(";") === cookies.length - 1) {
+		document.cookie = cookies.concat(sameSiteRule);
+	} else {
+		document.cookie = cookies.concat(`; ${sameSiteRule}`);
+	}
+}
+initSameSiteRule();
+
 new Vue({
 	router,
 	store,
