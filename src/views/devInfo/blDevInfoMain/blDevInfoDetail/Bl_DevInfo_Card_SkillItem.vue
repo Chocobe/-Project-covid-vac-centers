d5e1960718d1492724eda2a74503d06b7783137e
김영우 <kyw05171@gmail.com>
2021-06-20 22:20:58 +0900

    <김영우>: "개발자 정보" 페이지의 "스킬 항목" 컴포넌트를 작성 하였습니다.

---
+++ src/views/devInfo/blDevInfoMain/blDevInfoDetail/Bl_DevInfo_Card_SkillItem.vue
@@ -0,0 +1,119 @@
+<template>
+	<div class="skillItem">
+		<div v-if="icon.length > 0" class="skillItem__icon">
+			<FontAwesomeIcon :icon="icon"></FontAwesomeIcon>
+		</div>
+
+		<h3 class="skillItem__title">
+			{{ title }}
+		</h3>
+
+		<div class="skillItem__contentWrapper">
+			<div class="skillItem__gaugeBar">
+				<Comp_GaugeBar :percent="percent"></Comp_GaugeBar>
+			</div>
+
+			<p class="skillItem__content">
+				<slot></slot>
+			</p>
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Comp_GaugeBar from "@/components/Comp_GaugeBar.vue";
+
+export default Vue.extend({
+	components: {
+		Comp_GaugeBar,
+	},
+
+	props: {
+		icon: {
+			type: Array,
+			default: () => {
+				return [];
+			},
+		},
+
+		title: {
+			type: String,
+			required: true,
+		},
+
+		percent: {
+			type: String,
+			default: "50%",
+		},
+	},
+});
+</script>
+
+<style scoped lang="scss">
+.skillItem {
+	@include flex(flex-start, flex-start);
+
+	margin-top: 20px;
+
+	position: relative;
+
+	&__icon {
+		@include width-height(50px);
+
+		position: absolute;
+		top: 0;
+		right: 0;
+
+		z-index: -1;
+
+		& > * {
+			@include width-height(100%);
+
+			color: $colors__purple_01;
+		}
+	}
+
+	&__title {
+		@include width-height(100px);
+
+		margin-bottom: 5px;
+		padding-left: 20px;
+
+		position: relative;
+
+		&::before {
+			@include width-height(5px, 5px);
+
+			content: "";
+
+			position: absolute;
+			top: 50%;
+			left: 7.5px;
+			transform: translateY(-50%);
+
+			background-color: $colors__blue_02;
+		}
+	}
+
+	&__contentWrapper {
+		@include width-height(100%);
+		min-width: 300px;
+
+		margin-left: 20px;
+
+		flex: 1;
+	}
+
+	&__gaugeBar {
+		@include width-height(100%);
+		max-width: 200px;
+	}
+
+	&__content {
+		margin-top: 5px;
+
+		word-break: keep-all;
+	}
+}
+</style>
