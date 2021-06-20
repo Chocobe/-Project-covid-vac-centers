7d9761927cf302cf82c4f074c8cb0a73fbacdef2
김영우 <kyw05171@gmail.com>
2021-06-20 22:22:39 +0900

    <김영우>: "개발자 정보" 페이지의 "세부정보" 컴포넌트를 작성 하였습니다.

---
+++ src/views/devInfo/blDevInfoMain/blDevInfoDetail/Bl_DevInfo_Detail.vue
@@ -0,0 +1,45 @@
+<template>
+	<article id="Bl_DevInfo_Detail" class="devDetail">
+		<!-- 자격증 -->
+		<Bl_DevInfo_Card_Cert></Bl_DevInfo_Card_Cert>
+
+		<!-- 경력 -->
+		<Bl_DevInfo_Card_Careers></Bl_DevInfo_Card_Careers>
+
+		<!-- 메인 스킬 -->
+		<Bl_DevInfo_Card_MainSkills></Bl_DevInfo_Card_MainSkills>
+
+		<!-- 서브 스킬 -->
+		<Bl_DevInfo_Card_SubSkills></Bl_DevInfo_Card_SubSkills>
+
+		<!-- 사용가능 툴 -->
+		<Bl_DevInfo_Card_Tools></Bl_DevInfo_Card_Tools>
+	</article>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Bl_DevInfo_Card_Cert from "@/views/devInfo/blDevInfoMain/blDevInfoDetail/Bl_DevInfo_Card_Cert.vue";
+import Bl_DevInfo_Card_Careers from "@/views/devInfo/blDevInfoMain/blDevInfoDetail/Bl_DevInfo_Card_Careers.vue";
+import Bl_DevInfo_Card_MainSkills from "@/views/devInfo/blDevInfoMain/blDevInfoDetail/Bl_DevInfo_Card_MainSkills.vue";
+import Bl_DevInfo_Card_SubSkills from "@/views/devInfo/blDevInfoMain/blDevInfoDetail/Bl_DevInfo_Card_SubSkills.vue";
+import Bl_DevInfo_Card_Tools from "@/views/devInfo/blDevInfoMain/blDevInfoDetail/Bl_DevInfo_Card_Tools.vue";
+
+export default Vue.extend({
+	components: {
+		Bl_DevInfo_Card_Cert,
+		Bl_DevInfo_Card_Careers,
+		Bl_DevInfo_Card_MainSkills,
+		Bl_DevInfo_Card_SubSkills,
+		Bl_DevInfo_Card_Tools,
+	},
+});
+</script>
+
+<style scoped lang="scss">
+.devDetail {
+	& > * {
+		margin-top: 20px;
+	}
+}
+</style>
