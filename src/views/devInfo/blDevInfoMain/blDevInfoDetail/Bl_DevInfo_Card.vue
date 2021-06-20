6a612740a71e0fce278093a85d6736bfb9ca8327
김영우 <kyw05171@gmail.com>
2021-06-20 22:21:19 +0900

    <김영우>: "개발자 정보" 페이지의 "카드" 컴포넌트를 작성 하였습니다.

---
+++ src/views/devInfo/blDevInfoMain/blDevInfoDetail/Bl_DevInfo_Card.vue
@@ -0,0 +1,41 @@
+<template>
+	<section class="blDevInfoCard">
+		<h1 class="blDevInfoCard__title">
+			{{ title }}
+		</h1>
+
+		<div class="blDevInfoCard__content">
+			<slot></slot>
+		</div>
+	</section>
+</template>
+
+<script>
+export default {
+	props: {
+		title: {
+			type: String,
+			required: true,
+		},
+	},
+};
+</script>
+
+<style scoped lang="scss">
+.blDevInfoCard {
+	padding: 20px 40px;
+
+	border-radius: 10px;
+	box-shadow: 1px 2px 6px rgba($colors__black_01, 0.5);
+
+	position: relative;
+
+	&__title {
+		margin-bottom: 10px;
+
+		color: $colors__black_01;
+		font-size: 1.25rem;
+		font-weight: 900;
+	}
+}
+</style>
