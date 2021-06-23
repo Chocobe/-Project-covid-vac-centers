35219142a1f48706d58658c33f4b45abb552a96b
김영우 <kyw05171@gmail.com>
2021-06-23 15:00:41 +0900

    <김영우>: "Storybook"에 ``Comp_Button.vue`` 컴포넌트를 추가 하였습니다.
    * 문서 카테고리: ``Components/Comp_Button``

---
+++ src/components/Comp_Button.stories.ts
@@ -0,0 +1,61 @@
+import { Meta, Story } from "@storybook/vue";
+import Comp_Button from "./Comp_Button.vue";
+import { action } from "@storybook/addon-actions";
+import { sbDescription } from "@/stories/sbUtils";
+
+export default {
+	title: "Components/Comp_Button",
+	component: Comp_Button,
+	argTypes: {
+		// Props
+		name: {
+			description: "``출력할 버튼명``",
+		},
+
+		// Events
+		click: {
+			description: sbDescription("click 이벤트", '@click="리스너"'),
+		},
+		enter: {
+			description: sbDescription("enter 이벤트", '@enter="리스너"'),
+		},
+		leave: {
+			description: sbDescription("leave 이벤트", '@leave="리스너"'),
+		},
+
+		// Event Listeners
+		onClick: {
+			description: '@click="onClick"',
+		},
+		onEnter: {
+			description: '@enter="onEnter"',
+		},
+		onLeave: {
+			description: '@leave="onLeave"',
+		},
+	},
+} as Meta;
+
+const template: Story = (args, { argTypes }) => ({
+	components: { Comp_Button },
+	props: Object.keys(argTypes),
+	template: `
+    <Comp_Button 
+      :name="name"
+      @click="onClick"
+			@enter="onEnter"
+			@leave="onLeave"
+    ></Comp_Button>
+  `,
+});
+
+const Basic = template.bind({});
+Basic.args = {
+	name: "내 버튼",
+	onClick: action("@click 이벤트"),
+	onEnter: action("@enter 이벤트"),
+	onLeave: action("@leave 이벤트"),
+};
+Basic.storyName = "기본";
+
+export { Basic };
